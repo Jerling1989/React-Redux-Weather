897c49199ab779bb2ccea9d568a8ac8202ff3d23
Jacob <jerling1989@gmail.com>
2018-05-23 03:40:43 -0400

    binding this context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,10 +3,28 @@ import React, { Component } from 'react';
 
 // CREATE SEARCHBAR CONTAINER
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		// INITIALIZE STATE
+		this.state = { term: '' };
+		// BIND THIS CONTEXT TO ONINPUTCHANGE
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+	// CREATE ONINPUTCHANGE FUNCTION
+	onInputChange(event) {
+		console.log(event.target.value);
+		this.setState({ term: event.target.value });
+	}
+	// RENDER COMPONENT METHOD
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input 
+					placeholder="Get a five-day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
