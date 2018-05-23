9a1341fdfaea8bd2f72cf78645dd41db6227cf44
Jacob <jerling1989@gmail.com>
2018-05-23 04:30:11 -0400

    setting up axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,20 @@
-// WEATHER API KEY
+// IMPORT AXIOS
+import axios from 'axios';
+
+// WEATHER API INFO
 const API_KEY = '43ddad3004e216a2b72d28d891fbe712';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+// APPLY TYPE TO VARIABLE
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// FETCHWEATHER ACTION FUNCTION
+export function fetchWeather(city) {
+	// SEARCH URL
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+	// RETURN
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+} 
