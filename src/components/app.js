521417f31411f0156bbeb73cd079fcbaeba97336
Jacob <jerling1989@gmail.com>
2018-05-23 03:13:28 -0400

    SearchBar container

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
+// IMPORT OBJECTS AND METHODS
 import React, { Component } from 'react';
+// IMPORT COMPONENTS AND CONTAINERS
+import SearchBar from '../containers/search_bar';
 
+// CREATE MAIN APP COMPONENT
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      	<SearchBar />
+      </div>
     );
   }
 }
