af995a5c1c4b1a76d0c548b46b90955a7a47855a
Jacob <jerling1989@gmail.com>
2018-05-25 02:28:37 -0400

    reducer comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
+// IMPORT METHOD FROM REDUX
 import { combineReducers } from 'redux';
-
+// IMPORT REDUCERS
 import WeatherReducer from './reducer_weather';
 
+// CREATE COMBINED REDUCER
 const rootReducer = combineReducers({
   weather: WeatherReducer
 });
 
+// EXPORT COMBINED REDUCER
 export default rootReducer;
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,13 +1,15 @@
-
+// IMPORT FECTH WEATHER VARIABLE
 import { FETCH_WEATHER } from '../actions/index';
 
+// CREATE REDUCER FUNCTION
 export default function(state = [], action) {
-
+	// CHECK FOR ACTION TYPE
 	switch (action.type) {
 		case FETCH_WEATHER:
-			// RETURN NEW STATE ARRAY BY ADDING ACTION DATA INTO CURRENT STATE ARRAY
+			// CREATE NEW STATE ARRAY BY ADDING ACTION DATA INTO CURRENT STATE ARRAY
 			return [ action.payload.data, ...state ];
 	}
 
+	// RETURN STATE
 	return state;
 }
