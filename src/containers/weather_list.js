16bd85e8a22c5f699b98016abca3134bae791309
Jacob <jerling1989@gmail.com>
2018-05-25 04:56:49 -0400

    labeling chart data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,16 +8,19 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
 	// CREATE RENDERWEATHER FUNCTION
 	renderWeather(cityData) {
+		// CREATE WEATHER DATA VARIABLES
 		const name = cityData.city.name;
-		const temps = cityData.list.map(weather => weather.main.temp);
+		const temps = cityData.list.map(weather => ((weather.main.temp - 273.15) * 1.8) + 32);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
 
 		// RETURN WEATHER DATA
 		return (
-			<tr key={name}>
-				<td>{name}</td>
-				<td>
-					<Chart data={temps} color="orange" />
-				</td>
+			<tr key={name} className="row">
+				<td className="col-xs-3">{name}</td>
+				<td className="col-xs-3"><Chart data={temps} color="orange" units="&deg;F" /></td>
+				<td className="col-xs-3"><Chart data={pressures} color="green" units="hPa" /></td>
+				<td className="col-xs-3"><Chart data={humidities} color="black" units="%" /></td>
 			</tr>
 		);
 	}
@@ -26,11 +29,11 @@ class WeatherList extends Component {
 		return (
 			<table className="table table-hover">
 				<thead>
-					<tr>
-						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+					<tr className="row">
+						<th className="col-xs-3">City</th>
+						<th className="col-xs-3">Temperature (&deg;F)</th>
+						<th className="col-xs-3">Pressure (hPa)</th>
+						<th className="col-xs-3">Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
